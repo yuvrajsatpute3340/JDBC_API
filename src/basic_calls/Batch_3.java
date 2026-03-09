79e73036b30c64f76f05a5fd404d3ced8fa91dcc
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-03-09 16:03:17 +0530

    Jdbc

---
+++ src/basic_calls/Batch_3.java
@@ -0,0 +1,52 @@
+package basic_calls;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Batch_3 {
+
+    public static void main(String[] args) throws SQLException {
+
+        try (
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/batch", "root", "Yuvraj@3340");
+
+            PreparedStatement stmt = con.prepareStatement(
+                    "INSERT INTO Studentt VALUES(?,?,?)");
+
+            Scanner scan = new Scanner(System.in);
+        ) {
+
+            con.setAutoCommit(false);
+
+            for (int i = 0; i < 5; i++) {
+
+                System.out.println("Enter ID:");
+                int id = scan.nextInt();
+
+                System.out.println("Enter Name:");
+                String name = scan.next();
+
+                System.out.println("Enter Email:");
+                String email = scan.next();
+
+                stmt.setInt(1, id);
+                stmt.setString(2, name);
+                stmt.setString(3, email);
+
+                stmt.addBatch();
+            }
+
+            int[] arr = stmt.executeBatch();
+
+            for (int i : arr) {
+                System.out.println("Rows affected: " + i);
+            }
+
+            con.commit();
+        }
+    }
+}
